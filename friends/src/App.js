11ba58b0e01f3889b3718a795255173438039d80
Annette Odingo <odingoannette@gmail.com>
2019-12-17 12:38:44 +0000

    Add Route for login page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login';
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       Hello
+      <Route exact path= '/' component= {Login}/>
     </div>
   );
 }
