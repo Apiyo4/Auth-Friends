e2b1bea09ae42815f88165628f065d44844a00bd
Annette Odingo <odingoannette@gmail.com>
2019-12-17 12:59:33 +0000

    Add functionality for form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function Login(props){
     console.log(props)
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    
+    const submit = ()=>{
+        axios.post('http://localhost:5000/login',
+        {
+         username: usernameRef.current.value,
+         password: passwordRef.current.value   
+        })
+        
+       
+    }
     return(
         <div>
-           <form > 
+           <form onSubmit={submit}> 
             <label>Username:
-                <input type='text' />
+                <input type='text' ref ={usernameRef} />
             </label>
             <br/>
             <label>Password:
-                <input type='text'/>
+                <input type='text' ref={passwordRef}/>
             </label>
             <br/>
+            
             <button type='submit'>Login</button>
            </form>
         </div>
