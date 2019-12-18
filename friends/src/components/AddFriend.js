b9b9f022f6402a88d94176c708a9c4614cfb92f7
Annette Odingo <odingoannette@gmail.com>
2019-12-18 05:06:59 +0000

    Create a form to add a friend

--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from '../axios';
+function AddFriend(props){
+    const [info, setInfo] = useState({
+        name: '',
+        age: '',
+        email: ''
+    });
+    const handleChange= e=>{
+        setInfo({
+            ...info,
+            [e.target.name] : e.target.value,
+        })
+        
+    }
+    const submit = e =>{
+        e.preventDefault()
+        axios().post('http://localhost:5000/api/friends', info)
+        .then(res=>{
+            console.log(res.data);
+        })
+        .catch(er=>{
+            alert(er.message)
+        }
 
-function AddFriend(){
+        )
+        props.history.push('/friends')
+    }
     return(
         <div>
             <form>
                 <label>Name:
-                    <input type='text' name= 'name'/>
+                    <input type='text' name='name'  value= {info.name} onChange={handleChange}/>
                 </label>
                 <br />
                 <label>Age:
-                    <input type='text' name= 'age'/>
+                    <input type='text' name='age' value= {info.age} onChange={handleChange}/>
                 </label>
                 <br />
                 <label>Email:
-                    <input type='text' name= 'email'/>
+                    <input type='text' name='email' value={info.email} onChange={handleChange}/>
                 </label>
                 <br />
-                <button>Add Friend</button>
+                <button onClick={submit}>Add Friend</button>
             </form>
         </div>
     )
