a7fdbf674bac4bdd782d1527e91d4ff3b2706a76
Annette Odingo <odingoannette@gmail.com>
2019-12-18 04:29:31 +0000

    Create a form to add a friend

--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -3,7 +3,21 @@ import React from 'react';
 function AddFriend(){
     return(
         <div>
-            Hello
+            <form>
+                <label>Name:
+                    <input type='text' name= 'name'/>
+                </label>
+                <br />
+                <label>Age:
+                    <input type='text' name= 'age'/>
+                </label>
+                <br />
+                <label>Email:
+                    <input type='text' name= 'email'/>
+                </label>
+                <br />
+                <button>Add Friend</button>
+            </form>
         </div>
     )
 }
